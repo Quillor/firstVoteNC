7db40f0740b75fda4d431cb8a3fc5f68c2e9d411
Charley Birondo <charls_637@yahoo.com>
2017-08-28 13:55:43 +0800

    HomePage Update and Email Update

---
+++ wp-content/themes/landing/templates/components/banner-header-home.php
@@ -0,0 +1,15 @@
+<?php
+
+use Roots\Sage\Assets;
+
+?>
+<section class="jumbotron">
+  <div class="parallax-img hidden-xs" style="background-image:url('<?php echo Assets\asset_path('images/voting.png'); ?>')"></div>
+  <div class="container text-center">
+    <img class="visible-xs-block" src="<?php echo Assets\asset_path('images/banner-flag-xs.jpg'); ?>" alt="" />
+    <h1>Create a real election experience for your students!</h1>
+    <p><em>First vote is a realistic simulation of the voting process.</em></p>
+    <p class=""><a class="btn btn-primary btn-lg" href="/sign-up/" role="button" title="Sign up!">SIGN UP TODAY</a></p>
+    <p class="mt-5 mb-5">Sign up closes on [October 15, 2017]</p>
+  </div>
+</section>
