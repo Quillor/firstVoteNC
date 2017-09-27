b8e00733015f8503388d37502f3a4271ecfb02f6
Charley Birondo <charls_637@yahoo.com>
2017-09-27 14:57:54 +0800

    HomePage Text Banner

--- wp-content/themes/landing/templates/components/banner-header-home.php
+++ wp-content/themes/landing/templates/components/banner-header-home.php
@@ -6,7 +6,10 @@ use Roots\Sage\Assets;
 <section class="jumbotron">
   <div class="parallax-img hidden-xs" style="background-image:url('<?php echo Assets\asset_path('images/voting.png'); ?>')"></div>
   <div class="container text-center">
-    <img class="visible-xs-block" src="<?php echo Assets\asset_path('images/banner-flag-xs.jpg'); ?>" alt="" />
+	<p><span style="font-size: 16pt; color: #9d0e2b;">Nominated for a 2017 national American Civic Collaboration (<a style="color: #9d0e2b;" href="http://www.civvys.org/">Civvys</a>) Award!</span></p>
+    <p></p>
+    <p><span style="font-size: 16pt; color: #9d0e2b;">Announcing the EducationNC $1,000 Civics Incentive! </span></p>
+    <p>Every public or charter North Carolina high school that signs up and holds a First Vote school-wide local election simulation will be eligible.  Post-election, First Vote will draw a school’s name out of the proverbial hat and send them a check for $1,000 to be used for civic education at their school.</p>
     <h1>Create an authentic civic experience for your students!</h1>
     <p>includes realistic election simulation, lesson plans, exit polls, and data comparisons with peers across the state</p>
     <p>Engaging, easy to use, and FREE</p>
